8ef1130a9dd7186a2dc13b33fe95e2092799e5ba
Sophia Nasseri <nasseri.sophia@gmail.com>
2021-09-10 15:35:53 +0200

    Génération dynamique de la bannière pour la page du photographe

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ window.addEventListener("scroll", () => {
 
 let photographerData = [];
 
-const fetchPhotographer = async () => {
+export const fetchPhotographer = async () => {
   await fetch("./js/data.json")
     .then((res) => res.json())
     .then((data) => (photographerData = data.photographers));
@@ -33,14 +33,18 @@ const photographerDisplay = async () => {
           <a href="./photographer.html?id=${
             photographer.id
           }" class="photographer-link">
-            <img src="images/photographers/${photographer.portrait}" alt="" />
-            <h2 class="photographer__name">${photographer.name}</h2>
+            <img src="images/photographers/${
+              photographer.portrait
+            }" class="profile-image" alt="" />
+            <h2 class="photographer__name profile-name">${
+              photographer.name
+            }</h2>
           </a>
           <div class="photographer__description">
-            <p class="photographer__location">${photographer.city}, ${
-        photographer.country
-      }</p>
-            <p class="photographer__motto">${photographer.tagline}</p>
+            <p class="photographer__location profile-location">${
+              photographer.city
+            }, ${photographer.country}</p>
+            <p class="photographer__tagline">${photographer.tagline}</p>
            <p class="photographer__price">${photographer.price}€/jour</p>
           </div>
           <ul>
@@ -56,18 +60,11 @@ const photographerDisplay = async () => {
 
 photographerDisplay();
 
-//1. Sélectionner tous les tags
-//2. Ajouter event listener sur chacun des tags
-//3. Récupérer la valeur du tag
-//4. Récupérer tous les photographes
-
 const tagFilter = async () => {
   await fetchPhotographer();
   await photographerDisplay();
 
   const tagEl = document.querySelectorAll(".tag");
-
-  console.log(tagEl);
 
   tagEl.forEach((tags) => {
     tags.addEventListener("click", (e) => {
@@ -92,73 +89,42 @@ const tagFilter = async () => {
 };
 tagFilter();
 
-//Tags
+//Page photographe
+const photographerBannerDisplay = async () => {
+  await fetchPhotographer();
 
-// const photographerId = document.querySelectorAll(".photographer");
+  const getUrlId = new URLSearchParams(location.search).get("id");
+  const getPhotographerId = photographerData.find(
+    (element) => element.id == getUrlId
+  );
+  let tags = [];
+  for (let i = 0; i < getPhotographerId.tags.length; i++) {
+    tags.push(
+      `  <li><a href="#" class="tag" data-tag="${getPhotographerId.tags[i]}">#${getPhotographerId.tags[i]}</a></li>`
+    );
+  }
 
-// const tags = (tag) => {
-//   for (let i = 0; i < photographerId.length; i++) {
-//     if (photographerId[i].dataset.tags.includes(tag)) {
-//       photographerId[i].style.display = "flex";
-//     } else {
-//       photographerId[i].style.display = "none";
-//     }
-//   }
-// };
-
-// const portraitTag = document.querySelectorAll("#portrait");
-
-// for (let i = 0; i < portraitTag.length; i++) {
-//   portraitTag[i].addEventListener("click", () => {
-//     tags("portrait");
-//   });
-// }
-// const artTag = document.querySelectorAll("#art");
-
-// for (let i = 0; i < artTag.length; i++) {
-//   artTag[i].addEventListener("click", () => {
-//     tags("art");
-//   });
-// }
-// const fashionTag = document.querySelectorAll("#fashion");
-
-// for (let i = 0; i < fashionTag.length; i++) {
-//   fashionTag[i].addEventListener("click", () => {
-//     tags("fashion");
-//   });
-// }
-// const architectureTag = document.querySelectorAll("#architecture");
-
-// for (let i = 0; i < architectureTag.length; i++) {
-//   architectureTag[i].addEventListener("click", () => {
-//     tags("architecture");
-//   });
-// }
-// const travelTag = document.querySelectorAll("#travel");
-
-// for (let i = 0; i < travelTag.length; i++) {
-//   travelTag[i].addEventListener("click", () => {
-//     tags("travel");
-//   });
-// }
-// const sportTag = document.querySelectorAll("#sport");
-
-// for (let i = 0; i < sportTag.length; i++) {
-//   sportTag[i].addEventListener("click", () => {
-//     tags("sport");
-//   });
-// }
-// const animalsTag = document.querySelectorAll("#animals");
-
-// for (let i = 0; i < animalsTag.length; i++) {
-//   animalsTag[i].addEventListener("click", () => {
-//     tags("animals");
-//   });
-// }
-// const eventsTag = document.querySelectorAll("#events");
-
-// for (let i = 0; i < eventsTag.length; i++) {
-//   eventsTag[i].addEventListener("click", () => {
-//     tags("events");
-//   });
-// }
+  document.querySelector(".photographer-banner").innerHTML = `
+      
+  <div class="photographer-banner__description">
+  <h1 class="photographer-banner__name profile-name">${
+    getPhotographerId.name
+  }</h1>
+  <div>
+      <p class="photgrapher-banner__location profile-location">${
+        getPhotographerId.city
+      }, ${getPhotographerId.country}</p>
+      <p class="photgrapher-banner__tagline">${getPhotographerId.tagline}</p>
+  </div>
+   <ul>
+       <li><a href="#" class="tags" data-tags=""></a>${tags.join("")}</li>
+       <span></span>
+   </ul>
+ </div>
+ <button class="modal-btn">Contactez-moi</button>
+ <img src="images/photographers/${
+   getPhotographerId.portrait
+ }" class="profile-image" alt="">
+ `;
+};
+photographerBannerDisplay();
--- js/photographer.js
+++
@@ -1,9 +0,0 @@
-let photographerData = [];
-
-const fetchPhotographer = async () => {
-  await fetch("./js/data.json")
-    .then((res) => res.json())
-    .then((data) => (photographerData = data.photographers));
-};
-
-const getUrlId = new URLSearchParams(location.search).get("id");
