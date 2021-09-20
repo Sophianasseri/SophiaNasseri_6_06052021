39af694d7cb206e944c0b6af97efaf98fab8c5d7
Sophia Nasseri <nasseri.sophia@gmail.com>
2021-09-20 15:52:06 +0200

    Validation du formulaire

--- js/modal.js
+++ js/modal.js
@@ -13,7 +13,7 @@ const createModal = async () => {
     (element) => element.id == pageId
   );
   modal.innerHTML = `
-<form
+           <form
               id="contact"
               name="contact"
               action="photographer.html"
@@ -27,31 +27,35 @@ const createModal = async () => {
               <div class="form-data">
                 <label for="first">Prénom</label>
                 <input type="text" id="first" name="first" />
+                <small></small>
               </div>
               <div class="form-data">
                 <label for="last">Nom</label>
                 <input type="text" id="last" name="last" />
+                <small></small>
               </div>
-            </form>
-            <div class="form-data">
-              <label for="email">Email</label>
-              <input type="email" id="email" name="email" />
-            </div>
-            <div class="form-data">
-              <label for="message">Votre message</label>
-              <textarea
+              <div class="form-data">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" />
+                <small></small>
+              </div>
+              <div class="form-data">
+                <label for="message">Votre message</label>
+                <textarea
                 name="message"
                 id="message"
                 cols="30"
                 rows="5"
-              ></textarea>
-            </div>
-            <input type="submit" value="Envoyer" class="btn contact-btn" />
-`;
+                ></textarea>
+                <small></small>
+              </div>
+              <input type="submit" value="Envoyer" class="btn contact-btn" />
+          </form>
+  `;
 };
 createModal();
 
-//Ouvrir la modale
+//Ouvrir et fermer la modale
 
 const modaldisplay = async () => {
   await createModal();
@@ -69,3 +73,125 @@ const modaldisplay = async () => {
   });
 };
 modaldisplay();
+
+//Validation formulaire
+const validateForm = async () => {
+  await modaldisplay();
+  const modalBg = document.querySelector(".modal-background");
+  const form = document.getElementById("contact");
+  const firstNameEl = document.getElementById("first");
+  const lastNameEl = document.getElementById("last");
+  const emailEl = document.getElementById("email");
+  const messageEl = document.getElementById("message");
+
+  const isrequired = (value) => (value === "" ? false : true);
+
+  const showError = (input, message) => {
+    const parentEl = input.parentElement;
+    input.classList.add("error");
+
+    const error = parentEl.querySelector("small");
+    error.textContent = message;
+  };
+
+  const showSuccess = (input) => {
+    const parentEl = input.parentElement;
+    input.classList.remove("error");
+    const error = parentEl.querySelector("small");
+    error.textContent = "";
+  };
+  const checkFirstName = () => {
+    let valid = false;
+    const value = firstNameEl.value.trim();
+
+    if (!isrequired(value)) {
+      showError(firstNameEl, "Veuillez entrer un prénom");
+    } else if (value.length < 2) {
+      showError(firstNameEl, "Veillez entrer 2 caractères minimum");
+    } else {
+      valid = true;
+      showSuccess(firstNameEl);
+    }
+    return valid;
+  };
+  const checkLastName = () => {
+    let valid = false;
+    const value = lastNameEl.value.trim();
+    if (!isrequired(value)) {
+      showError(lastNameEl, "Veuillez entrer un nom");
+    } else if (value.length < 2) {
+      showError(lastNameEl, "Veillez entrer 2 caractères minimum");
+    } else {
+      showSuccess(lastNameEl);
+      valid = true;
+    }
+    return valid;
+  };
+  const emailValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/;
+
+  const checkEmail = () => {
+    let valid = false;
+    const value = emailEl.value.trim();
+    if (!isrequired(value)) {
+      showError(emailEl, "Veuillez entrer un e-mail");
+    } else if (!value.match(emailValid)) {
+      showError(emailEl, "Veillez entrer un e-mail valide");
+    } else {
+      showSuccess(emailEl);
+      valid = true;
+    }
+    return valid;
+  };
+
+  const checkMessage = () => {
+    let valid = false;
+    const value = messageEl.value.trim();
+    if (!isrequired(value)) {
+      showError(messageEl, "Ce champ ne peut être vide");
+    } else {
+      showSuccess(messageEl);
+      valid = true;
+    }
+    return valid;
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const firstNameValidation = checkFirstName();
+    const lastNameValidation = checkLastName();
+    const emailValidation = checkEmail();
+    const messageValidation = checkMessage();
+
+    const formValidation =
+      firstNameValidation &&
+      lastNameValidation &&
+      emailValidation &&
+      messageValidation;
+
+    if (formValidation) {
+      modal.style.display = "none";
+      modalBg.style.display = "none";
+      form.reset();
+    }
+  });
+
+  form.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "first":
+        checkFirstName();
+        break;
+      case "last":
+        checkLastName();
+        break;
+      case "email":
+        checkEmail();
+        break;
+      case "message":
+        checkMessage();
+        break;
+    }
+  });
+};
+
+validateForm();
